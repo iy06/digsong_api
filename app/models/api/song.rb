629c14606ba8cb7943a3fdddfd74c5f8870719cd
iy06 <yuki.inoue@di-v.co.jp>
2021-04-07 18:25:26 +0900

    songモデルにバリデーションメモを記載

--- app/models/api/song.rb
+++ app/models/api/song.rb
@@ -1,4 +1,12 @@
 class Api::Song < ApplicationRecord
   # active_storageを使って音楽ファイルを保存する。
   has_one_attached :song_data
+
+  # バリデーション
+  # with_options presence: true do
+  #   validates :title
+  #   validates :key
+  #   validates :bpm
+  #   validates :song_data
+  # end
 end
