61383b4fef770060d7844eab8b8e15725a8c604b
iy06 <yuki.inoue@di-v.co.jp>
2021-04-07 17:59:58 +0900

    ルーティングの編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # apiルーティング
   namespace :api do
-    resorces :songs, only: [:index, :create, :update, :destroy]
+    resources :songs, only: [:index, :create, :update, :destroy]
   end
 end
