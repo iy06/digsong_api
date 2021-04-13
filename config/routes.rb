02db26b7cdb5159a251f48842357bfb4e9d82dab
iy06 <yuki.inoue@di-v.co.jp>
2021-04-13 19:09:34 +0900

    デフォルトのformatをjsonに指定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # apiルーティング
   namespace :api do
-    resources :songs, only: [:index, :create, :update, :destroy]
+    resources :songs, only: [ :index, :create, :update, :destroy ], defaults: { format: 'json' }
   end
 end
