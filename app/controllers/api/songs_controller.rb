d5d91765c050aac791ea12d5105e9e4d458e754d
iy06 <yuki.inoue@di-v.co.jp>
2021-04-21 18:33:20 +0900

    画像データ音楽データがあればattachする修正

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -5,8 +5,19 @@ class Api::SongsController < ApplicationController
 
   def create
     @song = Song.new(song_params)
+    # 画像データがあればattachする
+    if params[:image] != ""
+      @song.image.attach(params[:image])
+    end
+    # 音楽データがあればattachする
+    if params[:song_data] != ""
+      @song.song_data.attach(params[:song_data])
+    end
     if @song.save
       all_songs
+    else
+      @error_messages = @song.errors.messages
+      error_songs
     end
   end
 
@@ -21,13 +32,17 @@ class Api::SongsController < ApplicationController
   private
 
   def song_params
-    params.permit(:title, :key, :bpm, :image, :song_data)
+    params.permit(:title, :key, :bpm)
   end
 
   def all_songs
     @songs = Song.order('created_at DESC')
     # songs.json.jbuilderを返す
     render :songs
+  end
+
+  def error_songs
+    render :error_message, status: 422
   end
 
   def select_song
