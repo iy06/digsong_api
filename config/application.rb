6fe0b16ea76805f53b75e8fc16ef4a50caae6406
iy06 <yuki.inoue@di-v.co.jp>
2021-04-21 20:44:01 +0900

    application.rbのオートローダーの設定をコメントアウトしておく

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,8 @@ module DigSongApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.i18n.default_locale = :ja
-
+    # オートローダークラシックの設定
+    # config.autoloader = :classic
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
