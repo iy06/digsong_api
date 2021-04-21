070aa6e1696455ae70275d6ea84c51757c3011fa
iy06 <yuki.inoue@di-v.co.jp>
2021-04-21 18:37:01 +0900

    バリデーションとカスタムバリデーションの追加

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,10 +4,33 @@ class Song < ApplicationRecord
   # active_storageを使って音楽ファイルを保存する。
   has_one_attached :song_data
   # バリデーション
-  # with_options presence: true do
-  #   validates :title
-  #   validates :key
-  #   validates :bpm
-  #   validates :song_data
-  # end
+  with_options presence: true do
+    validates :title
+    validates :key
+    validates :bpm
+    # validates :image
+    # validates :song_data
+    validates :image_presence
+    validates :song_data_presence
+  end
+  # 画像データがattachされているか、content_typeが正常かのカスタムバリデーション
+  def image_presence
+    if image.attached?
+      if !image.content_type.in?(%('image/jpeg image/png'))
+        errors.add(:image, 'にはjpegまたはpngファイルを添付してください')
+      end
+    else
+      errors.add(:image, 'ファイルを添付してください')
+    end
+  end
+  # 音楽データがattachされているか、content_typeが正常かのカスタムバリデーション
+  def song_data_presence
+    if song_data.attached?
+      if !song_data.content_type.in?(%('audio/mpeg'))
+        errors.add(:song_data, 'にはmp3ファイルを添付してください')
+      end
+    else
+      errors.add(:song_data, 'ファイルを添付してください')
+    end
+  end
 end
