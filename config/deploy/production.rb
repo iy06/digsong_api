54fb90401c86f0197c0825066a576721398e427f
iy06 <yuki.inoue@di-v.co.jp>
2021-04-22 16:21:01 +0900

    production.rbの編集

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,4 @@
+server '13.113.50.22', user: 'ec2-user', roles: %w{app db web}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
